71f4c324590009f80fd0926750dfa3fe4a6b2057
Ignacio-Us <i.uribe01@ufromail.cl>
2024-04-23 15:37:41 -0400

    Add ListaQuehaceres.vue

--- vue-basico/src/App.vue
+++ vue-basico/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ListaQuehaceres/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListaQuehaceres from './components/ListaQuehaceres';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListaQuehaceres
   }
 }
 </script>
---
+++ vue-basico/src/components/ListaQuehaceres.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <input v-model="title" placeholder="Ingrese el nombre de la lista">
+        <br><br>
+        <h3>Ingrese una nueva tarea</h3>
+        <input v-model="task" placeholder="Ingrese una nueva tarea" />
+        <button v-on:click="addTask">+</button>
+
+        <ul>
+            <li v-for="t in lista" :key="t">{{ t }} <button v-on:click="removeTask">X</button></li>
+            
+        </ul>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'ListaQuehaceres',
+    data() {
+        return {
+            title: '',
+            task: '',
+            lista: []
+        }
+    },
+    methods: {
+        addTask() {
+            this.lista.push(this.task);
+        },
+        removeTask() {
+            delete this.lista[this.lista.indexOf(this.task)];
+            this.lista.pop();
+        }
+    }
+}
+</script>
